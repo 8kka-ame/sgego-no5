09067398071e8306afde050eab4e4076c2c77826
8kka <8kkadrop@gmail.com>
2020-01-30 15:25:17 +0900

    hole: 02_animation

---
+++ hole/02_animation/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+
+	characterWidth  = 64
+	characterHeight = 108
+
+	interval = 10 // スプライトの変更間隔
+)
+
+var (
+	frame     = 0 // 現在のフレーム数
+	spriteNum = 0 // gopher画像の番号 (左から0, 1, 2, 3)
+	character *ebiten.Image
+)
+
+func init() {
+	var err error
+	character, _, err = ebitenutil.NewImageFromFile("./img/gopher.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func update(screen *ebiten.Image) error {
+	frame++
+
+	// TODO: 10フレーム間隔で画像を変更する
+	// TODO: x0, y0, x1, y1を指定してスプライト画像を矩形で区切る
+	// x0 := ...
+	// y0 := ...
+	// x1 := ...
+	// y1 := ...
+	// gopher := image.Rect(x0, y0, x1, y1)
+
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	// TODO: SubImageを使用して矩形領域の画像を表示する
+
+	return nil
+}
+
+func main() {
+	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "02. Animation"); err != nil {
+		log.Fatal(err)
+	}
+}
